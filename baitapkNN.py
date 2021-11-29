26d8e76aa561ff851e9e52671201bef42ea03a1d
Hà Minh Trí <90489206+LouisTris@users.noreply.github.com>
2021-11-29 10:26:01 +0700

    Update baitapkNN.py

--- baitapkNN.py
+++ baitapkNN.py
@@ -5,41 +5,46 @@ from sklearn.model_selection import train_test_split
 
 
 def fitness_analyser(height, weight):
-    # Sample data
+    # Data mẫu
     overweight_people = [[100, 90], [120, 100], [200, 175], [80, 100], [40, 60]]
     fit_people = [[60, 175], [90, 190], [80, 180], [50, 140], [30, 120]]
 
     people = fit_people + overweight_people
     is_fit = [1, 1, 1, 1, 1, 0, 0, 0, 0, 0]
 
-    # Input and outputs
+    # Đầu vào và đầu ra
     X = np.array(people)
     y = np.array(is_fit)
 
-    # Split the data into testing and training data
+    # Chia dữ liệu thành dữ liệu thử nghiệm và đào tạo
     train_X, test_X, train_y, test_y = train_test_split(X, y, random_state=0, test_size=.2)
 
-    # Create and fit the model
+    # Tạo và điều chỉnh mô hình
     model = KNeighborsClassifier(n_neighbors=3)
     model.fit(train_X, train_y)
 
-    # Print the score of the model
+    # In điểm của mô hình
     # print('Score: ', model.score(test_X, test_y))
 
-    # Add your own values to predict
+    # Thêm các giá trị của riêng bạn để dự đoán
+
     prediction = model.predict([[weight, height]])
 
-    # Translate the result into something a human could read
+    # Dịch kết quả thành thứ mà con người có thể đọc được
+
     print(f'Prediction: {prediction[0]}', 'Fit' if int(prediction[0]) == 1 else 'Fat')
 
-    # Splits the data to be plotted
+    # Tách dữ liệu được vẽ biểu đồ
+
     ow_scatter = [np.array(overweight_people)[:, 0], np.array(overweight_people)[:, 1]]
     f_scatter = [np.array(fit_people)[:, 0], np.array(fit_people)[:, 1]]
-    # Scatters the data in colour
+    # Phân tán dữ liệu bằng màu sắc
+
     plt.scatter(ow_scatter[0], ow_scatter[1], color='r')
     plt.scatter(f_scatter[0], f_scatter[1], color='g')
 
-    # Plots the input
+    # Vẽ sơ đồ đầu vào
+
     plt.scatter(weight, height, color='black', s=100)
     plt.ylabel('Height')
     plt.xlabel('Weight')
